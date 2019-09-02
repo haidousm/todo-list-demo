9804c70ed761d258ca4865e61f7ddd13c723bed2
haidousm <haidous.m@gmail.com>
2019-09-02 18:54:21 +0300

    connect to mongodb using mongoose

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
 let itemsList = []
 
 app.set("view engine", "ejs");
-
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+
+mongoose.connect("mongodb://localhost:27017/todoListDB", {useNewUrlParser: true});
+
 
 app.get("/", (req, res) => {
 
